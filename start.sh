79bfa73ba2863085364dd88f5b02b300e6f22e0a
ƲƊƖƬƛƝƧӇƲ  ƧƛƦƘƛƦ <factyride@gmail.com>
2025-11-05 08:58:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,9 @@ echo "🔐 Starting SSH server..."
 service ssh start
 
 echo "🌐 Starting Ngrok TCP tunnel on port 22..."
-ngrok tcp 22 --log=stdout
+ngrok tcp 22 --log=stdout &
+sleep 5
+
+echo "📡 Fetching Ngrok public URL..."
+curl -s localhost:4040/api/tunnels | grep -o 'tcp://[0-9a-zA-Z\.:]*'
+wait
